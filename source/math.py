ad35788b7cc49044941dfba170ba2f579dc68fe1
Christoph Zorn <zornchrisjo@googlemail.com>
2018-11-15 21:07:38 +0100

    add geometry classes

---
+++ source/math.py
@@ -0,0 +1,58 @@
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+        return self
+
+    def __imul__(self, other):
+        self.x *= other
+        self.y *= other
+        return self
+
+    def __add__(self, other):
+        return Point(self.x + other.x,
+                     self.y + other.y)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return not (self == other)
+
+
+class Rect:
+    def __init__(self, x=0, y=0, w=0, h=0):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+        self.w += other.w
+        self.h += other.h
+        return self
+
+    def __imul__(self, other):
+        self.x *= other
+        self.y *= other
+        self.w *= other
+        self.h *= other
+        return self
+
+    def __add__(self, other):
+        return Rect(self.x + other.x,
+                    self.y + other.y,
+                    self.w + other.w,
+                    self.h + other.h)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.w == other.w and self.h == other.h
+
+    def __ne__(self, other):
+        return not (self == other)
