03c34ad65475b938b56f58b1e9a9127c3beb4380
styinx <zornchrisjo@googlemail.com>
2018-11-23 21:29:15 +0100

    add animations to gui elements

--- source/animation.py
+++ source/animation.py
@@ -1,19 +1,22 @@
-from source.aclib import ACLIB
-
-
 class Animation:
-    def __init__(self, target, var, start, step, stop):
+    def __init__(self, target, var, start, step, stop, loops=0, direction="Forwards"):
         self.var = var
         self.target = target
         self.start = start
         self.step = step
         self.stop = stop
 
-        self.direction = "Forwards"
-        self.loops = 0
+        self.direction = direction
+        self.loops = loops
         self.loop = 0
         self.progress = 0
         self.finished = False
+        self.valid = False
+
+        animation_var = getattr(self.target, self.var)
+        if hasattr(type(animation_var), "__add__") and hasattr(type(animation_var), "__imul__") and \
+                hasattr(type(animation_var), "__ne__") and hasattr(type(animation_var), "__eq__"):
+            self.valid = True
 
     def setDirection(self, direction):
         if direction in ["Forwards", "Backwards", "Alternate"]:
@@ -28,28 +31,33 @@ class Animation:
     def isFinished(self):
         return self.finished
 
+    def isValid(self):
+        return self.valid
+
     def init(self):
         if getattr(self.target, self.var) != self.start:
             setattr(self.target, self.var, self.start)
         return self
 
     def update(self, delta):
-        val = getattr(self.target, self.var)
+        if self.valid:
+            val = getattr(self.target, self.var)
 
-        if self.direction == "Forwards":
-            if val != self.stop:
-                setattr(self.target, self.var, (val + self.step))
-            else:
-                if self.loop >= self.loops:
-                    self.finished = True
-                self.loop += 1
-        if self.direction == "Alternate":
-            if self.progress == 0 or val != self.start:
-                if val == self.stop:
-                    self.step *= -1
-                setattr(self.target, self.var, (val + self.step))
-                self.progress = 1
-            else:
-                if self.loop >= self.loops:
-                    self.finished = True
-                self.loop += 1
+            if self.direction == "Forwards":
+                if val != self.stop:
+                    setattr(self.target, self.var, (val + self.step))
+                else:
+                    if self.loop >= self.loops:
+                        self.finished = True
+                    self.loop += 1
+            if self.direction == "Alternate":
+                if self.progress == 0 or val != self.start:
+                    if val == self.stop:
+                        self.step *= -1
+                    setattr(self.target, self.var, (val + self.step))
+                    self.progress = 1
+                else:
+                    if self.loop >= self.loops:
+                        self.finished = True
+                    self.loop += 1
+                    self.progress = 0
--- source/gui.py
+++ source/gui.py
@@ -1,14 +1,11 @@
+from os import stat
 import ac
 from source.color import Color
+from source.config import loadAppConfig
 from source.gl import Texture, rect, texture_rect
 from source.event import GUI_EVENT
+from source.math import Rect
 from source.animation import Animation
-
-
-class Point:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
 
 
 class Font:
@@ -35,8 +32,8 @@ class ACWidget(object):
         self._ac_obj = None
         self._child = None
         self._parent = None
-        self._pos = (0, 0)
-        self._size = (0, 0)
+        self._ac_size = (0, 0)
+        self._geometry = Rect()
         self._visible = True
         self._background_texture = None
         self._colored_texture = False
@@ -54,16 +51,23 @@ class ACWidget(object):
             self._parent = parent
             parent._child = self
 
-            if not isinstance(parent, ACApp):
-                self.setPos(parent.getPos())
-            else:
+            if isinstance(parent, ACApp):
                 self.setPos((0, 0))
+            elif isinstance(parent, ACWidget):
+                self.setPos(parent.getPos())
 
+            self._ac_size = parent.getSize()
             self.setSize(parent.getSize())
+        else:
+            self.setPos((0, 0))
 
     @staticmethod
     def getPosition(obj):
         return ac.getPosition(obj)
+
+    def onClick(self, func):
+        ac.addOnClickedListener(self._ac_obj, func)
+        return self
 
     def obj(self):
         return self._ac_obj
@@ -107,27 +111,40 @@ class ACWidget(object):
         if event in self._event_callback:
             self._event_callback[event]()
 
+    def getGeometry(self):
+        return self._geometry
+
+    def setGeometry(self, r):
+        self._geometry = r
+        self.setPos((r.x, r.y))
+        self.setSize((r.w, r.h))
+        return self
+
     def getPos(self):
-        return self._pos
+        return self._geometry.x, self._geometry.y
 
     def setPos(self, pos):
-        self._pos = pos
+        self._geometry.x = pos[0]
+        self._geometry.y = pos[1]
 
         if self._ac_obj is not None:
-            ac.setPosition(self._ac_obj, self._pos[0], self._pos[1])
+            ac.setPosition(self._ac_obj, self._geometry.x, self._geometry.y)
 
         self.dispatchEvent(GUI_EVENT.ON_POSITION_CHANGED)
 
         return self
 
     def getSize(self):
-        return self._size
+        return self._geometry.w, self._geometry.h
 
     def setSize(self, size):
-        self._size = size
+        self._geometry.w = size[0]
+        self._geometry.h = size[1]
+
+        self._ac_size = (self._geometry.w, self._geometry.h)
 
         if self._ac_obj is not None:
-            ac.setSize(self._ac_obj, self._size[0], self._size[1])
+            ac.setSize(self._ac_obj, self._geometry.w, self._geometry.h)
 
         self.dispatchEvent(GUI_EVENT.ON_SIZE_CHANGED)
 
@@ -235,14 +252,26 @@ class ACWidget(object):
             ac.setVisible(self._ac_obj, False)
 
     def addAnimation(self, animation):
-        if isinstance(animation, Animation):
+        if isinstance(animation, Animation) and animation.isValid():
             self._animation_queue.append(animation)
         return self
+
+    def hasAnimation(self):
+        return self._animation is not None
 
     def updateSize(self):
         pass
 
     def update(self, delta):
+        if self._child is not None:
+            self._child.update(delta)
+
+        if self._background:
+            col = self._background_color
+            if self._ac_obj is not None:
+                ac.setBackgroundColor(self._ac_obj, col.r, col.g, col.b)
+                ac.setBackgroundOpacity(self._ac_obj, col.a)
+
         if self._animation is None:
             if len(self._animation_queue) > 0:
                 self._animation = self._animation_queue.pop(0)
@@ -254,23 +283,28 @@ class ACWidget(object):
             else:
                 self._animation = None
 
-        if self._child is not None:
-            self._child.update(delta)
-
-        if self._background:
-            col = self._background_color
-            if self._ac_obj is not None:
-                ac.setBackgroundColor(self._ac_obj, col.r, col.g, col.b)
-                ac.setBackgroundOpacity(self._ac_obj, col.a)
-
         return self
 
     def render(self, delta):
+        if self._ac_obj is not None:
+            # if (self._geometry.x, self._geometry.y) != ac.getPosition(self._ac_obj):
+            #     self.dispatchEvent(GUI_EVENT.ON_POSITION_CHANGED)
+            #     if self._ac_obj is not None:
+            #         x, y = self.getPos()
+            #         ac.setPosition(self._ac_obj, x, y)
+
+            if (self._geometry.w, self._geometry.h) != self._ac_size:
+                self.dispatchEvent(GUI_EVENT.ON_SIZE_CHANGED)
+                self._ac_size = self.getSize()
+                if self._ac_obj is not None:
+                    ac.setSize(self._ac_obj, self._ac_size[0], self._ac_size[1])
+
+        r = self._geometry
         if self._border:
-            rect(self._pos[0], self._pos[1], self._size[0], self._size[1], self._border_color, False)
+            rect(r.x, r.y, r.w, r.h, self._border_color, False)
 
         if self._colored_texture:
-            texture_rect(self._pos[0], self._pos[1], self._size[0], self._size[1], self._background_texture, self._texture_color)
+            texture_rect(r.x, r.y, r.w, r.h, self._background_texture, self._texture_color)
 
         if self._child is not None:
             self._child.render(delta)
@@ -284,7 +318,7 @@ class DockingWidget(ACWidget):
 
         self.attachable = True
         self.attached = False
-        self.docking_area = (0, 0, 0, 0)
+        self.docking_area = Rect()
 
     def setAttachable(self, attachable):
         self.attachable = attachable
@@ -316,6 +350,8 @@ class ACApp(ACWidget):
         self._render_callback = None
         self._activated_callback = None
         self._dismissed_callback = None
+        self._config_file = ""
+        self._config_time = 0
 
         self._activated = self.activate
         self._dismissed = self.dismiss
@@ -337,6 +373,26 @@ class ACApp(ACWidget):
 
         self.drawBorder(0)
         self.setRenderCallback(self.render_callback)
+
+    def readConfig(self, filename):
+        try:
+            self._config_file = filename
+            self._config_time = stat(self._config_file).st_mtime
+            loadAppConfig(self, self._app_name, filename)
+        except IOError:
+            ac.console("Config file " + filename + " not found.")
+
+        return self
+
+    def configChanged(self):
+        if self._config_file != "":
+            last_changed = stat(self._config_file).st_mtime
+
+            if last_changed != self._config_time:
+                self._config_time = last_changed
+                self.dispatchEvent(GUI_EVENT.ON_CONFIG_CHANGED)
+                return True
+        return False
 
     def isMainApp(self):
         return self._main_app
@@ -432,8 +488,8 @@ class ACApp(ACWidget):
             x, y = app.getPos()
             w, h = app.getSize()
 
-            ax, ay = self._pos
-            aw, ah = self._size
+            ax, ay = self.getPos()
+            aw, ah = self.getSize()
 
             # horizontal
             if ax - x <= ay - y or ax + aw - x <= ay + ah - y:
@@ -494,16 +550,21 @@ class ACApp(ACWidget):
             ac.setBackgroundOpacity(self._ac_obj, col.a)
 
             x, y = ACWidget.getPosition(self._ac_obj)
+            nx, ny = self.getPos()
 
-            if self._pos[0] != x or self._pos[1] != y:
+            if nx != x or ny != y:
                 self.setPos((x, y))
                 self._position_changed = True
+
+        if self.configChanged():
+            self.readConfig(self._config_file)
 
         return self
 
     def render(self, delta):
         if self._border:
-            rect(0, 0, self._size[0], self._size[1], self._border_color, False)
+            s = self.getSize()
+            rect(0, 0, s[0], s[1], self._border_color, False)
 
         if self._child is not None:
             self._child.render(delta)
@@ -553,14 +614,14 @@ class ACHBox(ACBox):
     def addWidget(self, widget):
         super().addWidget(widget)
 
-        x = self._pos[0]
-        w = round(self._size[0] / self._children_count)
-        h = self._size[1]
+        x, y = self.getPos()
+        w, h = self.getSize()
+        nw = round(w / self._children_count)
 
         for c in self._children:
-            c.setPos((x, self._pos[1]))
-            c.setSize((w, h))
-            x += w
+            c.setPos((x, h))
+            c.setSize((nw, h))
+            x += nw
 
 
 class ACVBox(ACBox):
@@ -570,14 +631,14 @@ class ACVBox(ACBox):
     def addWidget(self, widget):
         super().addWidget(widget)
 
-        y = self._pos[1]
-        w = self._size[0]
-        h = round(self._size[1] / self._children_count)
+        x, y = self.getSize()
+        w, h = self.getSize()
+        nh = round(h / self._children_count)
 
         for child in self._children:
-            child.setPos((self._pos[0], y))
-            child.setSize((w, h))
-            y += h
+            child.setPos((x, y))
+            child.setSize((w, nh))
+            y += nh
 
 
 class ACGrid(ACLayout):
@@ -587,35 +648,39 @@ class ACGrid(ACLayout):
         self._cols = max(cols, 1)
         self._rows = max(rows, 1)
         self._children = [x[:] for x in [[0] * self._cols] * self._rows]
-        self._cell_width = round(self._size[0] / self._cols)
-        self._cell_height = round(self._size[1] / self._rows)
-
-        if isinstance(parent, ACApp):
-            self.setPos((0, 0))
+        self._dimension = [x[:] for x in [[(0, 0)] * self._cols] * self._rows]
+        self._cell_width = round(self._geometry.w / self._cols)
+        self._cell_height = round(self._geometry.h / self._rows)
 
     def getWidget(self, x, y):
         return self._children[y][x]
 
     def addWidget(self, widget, x, y, w=1, h=1):
         if isinstance(widget, ACWidget):
-            if isinstance(self._children[y][x], ACWidget):
-                del self._children[y][x]
             self._children[y][x] = widget
+            self._dimension[y][x] = (w, h)
 
-            widget.setPos((round(self._pos[0] + x * self._cell_width), round(self._pos[1] + y * self._cell_height)))
+            widget.setPos((round(self._geometry.x + x * self._cell_width),
+                           round(self._geometry.y + y * self._cell_height)))
             widget.setSize((round(w * self._cell_width), round(h * self._cell_height)))
+
+            widget.updateSize()
 
         return self
 
     def updateSize(self):
-        self._cell_width = round(self._size[0] / self._cols)
-        self._cell_height = round(self._size[1] / self._rows)
+        self._cell_width = round(self._geometry.w / self._cols)
+        self._cell_height = round(self._geometry.h / self._rows)
 
-        for row in self._children:
-            for cell in row:
+        for y, row in enumerate(self._children):
+            for x, cell in enumerate(row):
                 if isinstance(cell, ACWidget):
+                    cell.setPos(
+                        (round(self._geometry.x + x * self._cell_width),
+                         round(self._geometry.y + y * self._cell_height)))
+                    cell.setSize((round(self._dimension[y][x][0] * self._cell_width),
+                                  round(self._dimension[y][x][1] * self._cell_height)))
                     cell.updateSize()
-
         return self
 
     def update(self, delta):
@@ -636,6 +701,53 @@ class ACGrid(ACLayout):
                 if isinstance(cell, ACWidget):
                     cell.render(delta)
 
+        return self
+
+
+class ACLabelPair(ACGrid):
+    def __init__(self, app, parent=None, label=None, widget=None, label_pos="left"):
+        if label_pos == "left" or label_pos == "right":
+            super().__init__(parent, 2, 1)
+        elif label_pos == "top" or label_pos == "bottom":
+            super().__init__(parent, 1, 2)
+
+        self.label_widget = label
+        self.pair_widget = widget
+        self.label_position = label_pos
+
+        self.addLabelWidget(label)
+        self.addPairWidget(widget)
+
+    def addLabelWidget(self, label):
+        if isinstance(label, ACLabel):
+            self.label_widget = label
+
+            if self.label_position == "left" or self.label_position == "top":
+                self.addWidget(self.label_widget, 0, 0)
+            elif self.label_position == "right":
+                self.addWidget(self.label_widget, 1, 0)
+            elif self.label_position == "bottom":
+                self.addWidget(self.label_widget, 0, 1)
+
+        return self
+
+    def addPairWidget(self, widget):
+        if isinstance(widget, ACWidget):
+            self.pair_widget = widget
+
+            if self.label_position == "left":
+                self.addWidget(self.pair_widget, 1, 0)
+            elif self.label_position == "right" or self.label_position == "bottom":
+                self.addWidget(self.pair_widget, 0, 0)
+            elif self.label_position == "top":
+                self.addWidget(self.pair_widget, 0, 1)
+        return self
+
+    def render(self, delta):
+        if self.label_widget:
+            self.label_widget.render(delta)
+        if self.pair_widget:
+            self.pair_widget.render(delta)
         return self
 
 
@@ -683,7 +795,7 @@ class ACTextWidget(ACWidget):
         return self
 
     def setTextAlignment(self):
-        x, y = self._pos
+        x, y = self.getPos()
 
         # if self._text_h_alignment == "left":  # or self._text_h_alignment == "l":
         #     x = self._pos[0]
@@ -800,6 +912,11 @@ class ACLabel(ACTextWidget):
         self.setFontBold(self._font_bold)
 
 
+class ACButton(ACLabel):
+    def __init__(self, text, app, parent=None):
+        super().__init__(text, app, parent)
+
+
 class ACIcon(ACLabel):
     def __init__(self, path, app, parent=None):
         super().__init__("", app, parent)
@@ -812,7 +929,7 @@ class ACProgressBar(ACLabel):
         super().__init__("", app, parent)
 
         self._orientation = orientation
-        self._border = True
+        self._border = 1
         self._color = self._background_color
         self._h_margin = 1
         self._v_margin = 0.05
@@ -825,24 +942,37 @@ class ACProgressBar(ACLabel):
             self.v_margin = 1
 
     def render(self, delta):
+        x, y = self.getPos()
+        w, h = self.getSize()
+
         if self._orientation == 0:
-            v_margin = self._size[1] * self.v_margin
-            ratio = self._size[0] * (self._value / self._max_val)
-            rect(self._pos[0], self._pos[1] + v_margin, ratio, self._size[1] - 2 * v_margin, self._color)
+            v_margin = h * self.v_margin
+            ratio = w * (self._value / self._max_val)
+            rect(x, y + v_margin, ratio, h - 2 * v_margin, self._color)
 
             if self._border:
-                rect(self._pos[0], self._pos[1] + v_margin, self._size[0], self._size[1] - 2 * v_margin,
-                     self._border_color,
-                     False)
+                rect(x, y + v_margin, w, h - 2 * v_margin, self._border_color, False)
         else:
-            h_margin = self._size[1] * self.h_margin
-            ratio = self._size[1] * (self._value / self._max_val)
-            rect(self._pos[0] + h_margin, self._pos[1] + self._size[1] - ratio, self._size[0] - 2 * h_margin, ratio,
-                 self._color)
+            h_margin = h * self.h_margin
+            ratio = h * (self._value / self._max_val)
+            rect(x + h_margin, y + h - ratio, w - 2 * h_margin, ratio, self._color)
 
             if self._border:
-                rect(self._pos[0] + h_margin, self._pos[1], self._size[0] - 2 * h_margin, self._size[1],
-                     self._border_color,
-                     False)
+                rect(x + h_margin, y, w - 2 * h_margin, h, self._border_color, False)
 
         return self
+
+
+class ACSpinner(ACWidget):
+    def __init__(self, text, parent=None):
+        super().__init__(parent)
+
+
+class ACInput(ACWidget):
+    def __init__(self, text, parent=None):
+        super().__init__(parent)
+
+
+class ACGraph(ACWidget):
+    def __init__(self, text, parent=None):
+        super().__init__(parent)
