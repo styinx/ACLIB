dde2145ad358be9f819e622ff485ac4acfc84800
styinx <zornchrisjo@googlemail.com>
2019-01-19 13:30:57 +0100

    fix calculations for animations to make them work again

--- source/animation.py
+++ source/animation.py
@@ -1,3 +1,6 @@
+from source.aclib import ACLIB
+
+
 class Animation:
     def __init__(self, target, var, start, step, stop, loops=0, direction="Forwards"):
         self.var = var
@@ -6,21 +9,30 @@ class Animation:
         self.step = step
         self.stop = stop
 
-        self.direction = direction
+        self.direction = "Forwards"
         self.loops = loops
         self.loop = 0
         self.progress = 0
         self.finished = False
         self.valid = False
 
+        self.setDirection(direction)
+
         animation_var = getattr(self.target, self.var)
         if hasattr(type(animation_var), "__add__") and hasattr(type(animation_var), "__imul__") and \
                 hasattr(type(animation_var), "__ne__") and hasattr(type(animation_var), "__eq__"):
             self.valid = True
+            self.init()
+        else:
+            ACLIB.CONSOLE("Class " + str(animation_var) + " does not fulfill animation requirements.")
 
     def setDirection(self, direction):
-        if direction in ["Forwards", "Backwards", "Alternate"]:
-            self.direction = direction
+        directions = ["Forwards", "Backwards", "Alternate"]
+        if isinstance(direction, str):
+            if direction in directions:
+                self.direction = direction
+        elif isinstance(direction, int):
+            self.direction = directions[direction]
 
         return self
 
@@ -47,17 +59,20 @@ class Animation:
                 if val != self.stop:
                     setattr(self.target, self.var, (val + self.step))
                 else:
-                    if self.loop >= self.loops:
+                    if self.loop >= self.loops != -1:
                         self.finished = True
                     self.loop += 1
-            if self.direction == "Alternate":
-                if self.progress == 0 or val != self.start:
+                    self.init()
+
+            elif self.direction == "Alternate":
+                if val != self.start or self.progress == 0:
                     if val == self.stop:
                         self.step *= -1
                     setattr(self.target, self.var, (val + self.step))
                     self.progress = 1
                 else:
-                    if self.loop >= self.loops:
+                    if self.loop >= self.loops != -1:
                         self.finished = True
                     self.loop += 1
+                    self.step *= -1
                     self.progress = 0
