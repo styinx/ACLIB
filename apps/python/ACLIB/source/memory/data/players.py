7de5f708342bb62776a77d4ebfabb2890af6c440
styinx <zornchrisjo@googlemail.com>
2020-10-20 14:23:15 +0200

    Calculate lap time

--- apps/python/ACLIB/source/memory/data/players.py
+++ apps/python/ACLIB/source/memory/data/players.py
@@ -1,6 +1,7 @@
 import ac
+import acsys
 
-from apps.python.system import acsys
+from time import time
 from memory.data.server import Server
 
 
@@ -8,13 +9,17 @@ class Players:
     def __init__(self, server: Server):
         self._server = server
         self._player = 0
+        self._valid = {}
 
         self._lap_index = {}
+        self._lap_progress = {}
+        self._current_lap_time = {}
+        self._start_lap_time = {}
         self._pb_lap_time = {}
         self._gb_lap_time = float('inf')
 
         self._sector_index = {}
-        self._sector_time = {}
+        self._current_sector_time = {}
         self._pb_sector_time = {}
         self._gb_sector_time = [float('inf')] * 3
 
@@ -22,12 +27,17 @@ class Players:
         #self._mini_sector_time = {}
 
         for c in range(0, self._server.cars):
+            self._valid[c] = False
             self._lap_index[c] = 0
+            self._lap_progress[c] = 0.0
+            self._current_lap_time[c] = 0.0
+            self._start_lap_time[c] = time() * 1000
             self._pb_lap_time[c] = float('inf')
-            self._pb_sector_time[c] = [float('inf')] * 3
 
             self._sector_index[c] = 0
-            self._sector_time[c] = [0] * 3
+            self._current_sector_time[c] = [0.0] * 3
+            self._pb_sector_time[c] = [float('inf')] * 3
+
             #self._mini_sector_index[c] = 0
             #self._mini_sector_time[c] = {}
 
@@ -38,19 +48,20 @@ class Players:
 
     def update(self, delta: int):
         for c in range(0, self._server.cars):
-            if ac.isCarInPit(c):
+            if ac.isCarInPit(c) or not ac.isConnected(c):
                 continue
 
             lap = ac.getCarState(c, acsys.CS.LapCount)
             pos = ac.getCarState(c, acsys.CS.NormalizedSplinePosition)
+            invalid = ac.getCarState(c, acsys.CS.LapInvalidated) and self._valid[c]
             sec = int(pos * 3)
             #msec = int(pos * 12)
 
-            lap_time = ac.getCarState(c, acsys.CS.LapTime)
-
             if lap != self._lap_index[c]:
+                self._valid[c] = True
+                self._start_lap_time[c] = time() * 1000
                 best_lap = ac.getCarState(c, acsys.CS.BestLap)
-                if best_lap > 0:
+                if not invalid and best_lap > 0:
                     if best_lap < self._gb_lap_time:
                         self._gb_lap_time = best_lap
                         self._pb_lap_time[c] = best_lap
@@ -59,20 +70,22 @@ class Players:
 
                 self._lap_index[c] = lap
 
+            self._current_lap_time[c] = time() * 1000 - self._start_lap_time[c]
+
             if sec > 0:
-                self._sector_time[c][sec] = lap_time - sum(self._sector_time[c][:sec])
+                self._current_sector_time[c][sec] = self._current_lap_time[c] - sum(self._current_sector_time[c][:sec])
             else:
-                self._sector_time[c][sec] = lap_time
+                self._current_sector_time[c][sec] = self._current_lap_time[c]
 
             if sec != self._sector_index[c]:
                 last_sec = self._sector_index[c]
 
-                if self._sector_time[c][last_sec] > 0:
-                    if self._sector_time[c][last_sec] < self._gb_sector_time[last_sec]:
-                        self._gb_sector_time[last_sec] = self._sector_time[c][last_sec]
-                        self._pb_sector_time[c][last_sec] = self._sector_time[c][last_sec]
-                    elif self._sector_time[c][last_sec] < self._pb_sector_time[c][last_sec]:
-                        self._pb_sector_time[c][last_sec] = self._sector_time[c][last_sec]
+                if not invalid and self._current_sector_time[c][last_sec] > 0:
+                    if self._current_sector_time[c][last_sec] < self._gb_sector_time[last_sec]:
+                        self._gb_sector_time[last_sec] = self._current_sector_time[c][last_sec]
+                        self._pb_sector_time[c][last_sec] = self._current_sector_time[c][last_sec]
+                    elif self._current_sector_time[c][last_sec] < self._pb_sector_time[c][last_sec]:
+                        self._pb_sector_time[c][last_sec] = self._current_sector_time[c][last_sec]
 
                 self._sector_index[c] = sec
 
@@ -133,7 +146,7 @@ class Players:
 
     def sector_time(self, sec: int):
         if 0 <= sec < 3:
-            return self._sector_time[self._player][sec]
+            return self._current_sector_time[self._player][sec]
         return 0
 
     def pb_sector_time(self, sec: int):
